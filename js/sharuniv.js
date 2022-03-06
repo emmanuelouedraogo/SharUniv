4f88f49f2ba136bb77a806d4e6c483bf90bc0b78
emmanuelouedraogo <emmanuelrhema.amjc@gmail.com>
2022-03-06 14:29:24 +0100

    Mise à jour html et css 2

--- js/sharuniv.js
+++ js/sharuniv.js
@@ -1,1 +1,14 @@
-alert('Bienvenue sur Sharing Universe!!!')
+
+let weightLabel = document.querySelector('label[for="weight"]');
+let weightInput = document.querySelector('#weight');
+let sampleText = document.querySelector('.sample');
+
+function update() {
+  weightLabel.textContent = `font-weight: ${weightInput.value};`;
+  sampleText.style.fontWeight = weightInput.value;
+}
+
+weightInput.addEventListener('input', update);
+
+update();
+
